865ef8cf7c72147fb74f4449db7fc5535d0df47b
alihajiali <alihajiali1379@gmail.com>
2024-05-24 10:47:28 +0330

    first commit

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-from fastapi import FastAPI
-import uvicorn
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+import uvicorn
 
 templates = Jinja2Templates(directory="templates/")
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 @app.get('/')
 def read_root(request: Request):
     return templates.TemplateResponse('index.html', context={'request': request})
-
-
 
 if __name__=="__main__":
     uvicorn.run("main:app",host='0.0.0.0', port=10003, reload=True, workers=3)
