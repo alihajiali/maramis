48ef3a5c121b60190f57bdd74ecb6fa2d7bc443d
Ali HajiAli <90006456+alihajiali@users.noreply.github.com>
2024-06-01 23:18:00 +0330

    commit

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ def read_root(request: Request):
     return templates.TemplateResponse('index.html', context={'request': request})
 
 if __name__=="__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=10003, reload=True, workers=3)
+    uvicorn.run("main:app",host='0.0.0.0', port=8000, reload=True, workers=3)
